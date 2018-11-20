00d2d63ba96b6de8fe4b44937053661744731623
ChungToan <chungtoan94@gmail.com>
2018-11-20 23:12:29 +0700

    edit file

--- manageSell/manageSell/Properties/Resources.Designer.cs
+++ manageSell/manageSell/Properties/Resources.Designer.cs
@@ -99,5 +99,25 @@ namespace manageSell.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap images__6_ {
+            get {
+                object obj = ResourceManager.GetObject("images (6)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap STAFF {
+            get {
+                object obj = ResourceManager.GetObject("STAFF", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
--- manageSell/manageSell/main.cs
+++ manageSell/manageSell/main.cs
@@ -12,24 +12,18 @@ namespace manageSell
 {
     public partial class main : Form
     {
-        sellForm sForm;
-        manageForm mForm;
-        warehouseForm wForm;
+        
         // liệt kê hàng hóa ra bảng 
         // tìm kiếm theo loại
         // tìm kiếm theo text. liệt kê đk số lượng tồn kho ,, 
         private void button1_Click(object sender, EventArgs e)
         {
-            sForm = new sellForm();
-            sForm.Visible = true;
-            mForm.Visible = false;
-            wForm.Visible = false;
+          
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-             wForm = new warehouseForm();
-
+          
         }
         public main()
         {
@@ -42,6 +36,21 @@ namespace manageSell
             // quan li loi nhuan ,, doanh so thang,
             // nha cung cap hang. 
             // nhap hang 
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
